8564862486b395894d6716e4393201a509356e0a
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2022-01-29 18:31:53 -0500

    Model Changes

--- listnr/admin.py
+++ listnr/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Listnr, Song, Review
 # Register your models here.
+admin.site.register(Listnr)
+admin.site.register(Song)
+admin.site.register(Review)
