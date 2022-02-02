a91c8bc9de9984793368322cdc4665feffa4e2d5
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2022-02-02 14:16:31 -0500

    installed axios

--- listnr-front/src/services/apiConfig.js
+++ listnr-front/src/services/apiConfig.js
@@ -1,26 +1,27 @@
 import axios from "axios";
 
 const getToken = () => {
-  return new Promise(resolve => {
-      resolve(`Bearer ${localStorage.getItem('token') || null}`)
-  })
-}
+  return new Promise((resolve) => {
+    resolve(`Bearer ${localStorage.getItem("token") || null}`);
+  });
+};
 
 const api = axios.create({
-  baseURL: "http://localhost:8000"
-   
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://listnr-database.herokuapp.com"
+      : "http://localhost:8000",
 });
 
-// process.env.NODE_ENV === "production"
-// ? "https://listnr-database.herokuapp.com"
-// : "http://localhost:8000"
-
-api.interceptors.request.use(async function (config) {
-  config.headers['Authorization'] = await getToken()
-  return config
-}, function (error) {
-  console.log('Request error: ', error)
-  return Promise.reject(error)
-});
+api.interceptors.request.use(
+  async function (config) {
+    config.headers["Authorization"] = await getToken();
+    return config;
+  },
+  function (error) {
+    console.log("Request error: ", error);
+    return Promise.reject(error);
+  }
+);
 
 export default api;
